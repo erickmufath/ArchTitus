f9e030a44e479fa4a36b3c9a049ed6b972b36b09
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-02-03 01:43:03 +0700

    Add files via upload

---
+++ 2-user.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+echo -ne "
+-------------------------------------------------------------------------
+   █████╗ ██████╗  ██████╗██╗  ██╗████████╗██╗████████╗██╗   ██╗███████╗
+  ██╔══██╗██╔══██╗██╔════╝██║  ██║╚══██╔══╝██║╚══██╔══╝██║   ██║██╔════╝
+  ███████║██████╔╝██║     ███████║   ██║   ██║   ██║   ██║   ██║███████╗
+  ██╔══██║██╔══██╗██║     ██╔══██║   ██║   ██║   ██║   ██║   ██║╚════██║
+  ██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ╚██████╔╝███████║
+  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+                        SCRIPTHOME: ArchTitus
+-------------------------------------------------------------------------
+
+Installing AUR Softwares
+"
+# You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
+source $HOME/ArchTitus/setup.conf
+
+addAUR () {
+  cd ~
+  git clone "https://aur.archlinux.org/$1.git"
+  cd ~/$1
+  makepkg -si --noconfirm
+  cd ~
+}
+
+if [${AURHELPER} -ne ""] then
+  addAUR ${AURHELPER}
+fi
+
+touch "~/.cache/zshhistory"
+git clone "https://github.com/ChrisTitusTech/zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+ln -s "~/zsh/.zshrc" ~/.zshrc
+
+for PACKAGE in $(cat ~/ArchTitus/pkg-files/aur-pkgs.txt) do
+  addAUR $PACKAGE
+esac
+
+export PATH=$PATH:~/.local/bin
+cp -r ~/ArchTitus/dotfiles/* ~/.config/
+echo -ne "
+-------------------------------------------------------------------------
+                    SYSTEM READY FOR 3-post-setup.sh
+-------------------------------------------------------------------------
+"
+exit
---
+++ 3-post-setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+echo -ne "
+-------------------------------------------------------------------------
+   █████╗ ██████╗  ██████╗██╗  ██╗████████╗██╗████████╗██╗   ██╗███████╗
+  ██╔══██╗██╔══██╗██╔════╝██║  ██║╚══██╔══╝██║╚══██╔══╝██║   ██║██╔════╝
+  ███████║██████╔╝██║     ███████║   ██║   ██║   ██║   ██║   ██║███████╗
+  ██╔══██║██╔══██╗██║     ██╔══██║   ██║   ██║   ██║   ██║   ██║╚════██║
+  ██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ╚██████╔╝███████║
+  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+                        SCRIPTHOME: ArchTitus
+-------------------------------------------------------------------------
+
+Final Setup and Configurations
+GRUB EFI Bootloader Install & Check
+"
+source /root/ArchTitus/setup.conf
+genfstab -U / >> /etc/fstab
+
+# set kernel parameter for decrypting the drive
+if [[ "${FS}" == "luks" ]]; then
+sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${encryped_partition_uuid}:ROOT root=/dev/mapper/ROOT %g" /etc/default/grub
+fi
+
+grub-install --target=i386-pc --boot-directory=/boot ${DISK}
+grub-install --target=x86_64-efi --efi-directory=/boot --boot-directory=/boot --removable --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "All set!"
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Cleaning 
+-------------------------------------------------------------------------
+"
+# Remove no password sudo rights
+sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+# Add sudo rights
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+rm -r /root/ArchTitus
+rm -r /home/$USERNAME/ArchTitus
+
+# Replace in the same state
+cd $pwd
