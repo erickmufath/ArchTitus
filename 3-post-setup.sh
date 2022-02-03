aa010f85895a24f1b2f7c6790537a9eac4941f56
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-02-03 22:28:06 +0700

    Update 3-post-setup.sh

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -23,6 +23,12 @@ if [[ "${FS}" == "luks" ]]; then
 sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${encryped_partition_uuid}:ROOT root=/dev/mapper/ROOT %g" /etc/default/grub
 fi
 
+systemctl enable cups.service
+ntpd -qg
+systemctl enable ntpd.service
+systemctl enable bluetooth
+systemctl enable sddm
+
 grub-install --target=i386-pc --boot-directory=/boot ${DISK}
 grub-install --target=x86_64-efi --efi-directory=/boot --boot-directory=/boot --removable --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
