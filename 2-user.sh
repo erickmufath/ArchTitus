eb21baac332ebad70ac6e24223e0625b177670f0
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-02-04 09:22:39 +0700

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -17,26 +17,17 @@ Installing AUR Softwares
 # You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
 source $HOME/ArchTitus/setup.conf
 
-addAUR () {
+
   cd ~
-  git clone "https://aur.archlinux.org/$1.git"
-  cd ~/$1
+  git clone "https://aur.archlinux.org/yay.git"
+  cd ~/yay
   makepkg -si --noconfirm
   cd ~
-}
-
-if [${AURHELPER} -ne ""] then
-  addAUR ${AURHELPER}
-fi
 
 touch "~/.cache/zshhistory"
 git clone "https://github.com/ChrisTitusTech/zsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 ln -s "~/zsh/.zshrc" ~/.zshrc
-
-for PACKAGE in $(cat ~/ArchTitus/pkg-files/aur-pkgs.txt) do
-  addAUR $PACKAGE
-esac
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 yay -S --noconfirm --needed - < ~/ArchTitus/pkg-files/aur-pkgs.txt
